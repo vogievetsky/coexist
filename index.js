f09452a6e9e8f451018654270615ffa961b36a69
Vadim Ogievetsky <vadimon@gmail.com>
2017-03-31 12:18:54 -0700

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require.extensions['.css'] = function (module: any, filename: string) {};
-require.extensions['.svg'] = function (module: any, filename: string) {
+require.extensions['.css'] = function() {};
+require.extensions['.svg'] = function() {
   module.exports = '<svg viewBox="0 0 19 19"><rect width=19 height=19 fill="red"></rect></svg>';
 };
